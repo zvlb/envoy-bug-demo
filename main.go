0fe61e484aa1bd06ee7014a322f07c5403a40cf8
zvlb <vl.zemtsov@gmail.com>
2024-06-27 13:25:50 +0300

    more
    Signed-off-by: zvlb <vl.zemtsov@gmail.com>

--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 	xdscache "github.com/kaasops/envoy-xds-controller/pkg/xds/cache"
 	"github.com/kaasops/envoy-xds-controller/pkg/xds/server"
 	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/durationpb"
 
 	clusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	endpointv3 "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
 	listenerv3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+
 	basicv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/basic_auth/v3"
+	oath2v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/oauth2/v3"
 	routerv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/router/v3"
 	hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	tlsv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+	matcherv3 "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
 	testv3 "github.com/envoyproxy/go-control-plane/pkg/test/v3"
 
 	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
@@ -30,8 +35,12 @@ const (
 	routeConfigName = "test-route"
 
 	basicAuthFilterName = "envoy.filters.http.basic_auth"
+	oauth2FilterName    = "envoy.filters.http.oauth2"
 	local_service       = "local_service"
 	ws_service          = "ws_service"
+
+	oauth2TokenPath = "/Users/zemtsov.v/Documents/git/envoy-bug-demo/token-secret.yaml"
+	oauth2HMACnPath = "/Users/zemtsov.v/Documents/git/envoy-bug-demo/hmac.yaml"
 )
 
 func main() {
@@ -68,6 +77,11 @@ func fillCache(cache *xdscache.Cache) error {
 		return err
 	}
 
+	keycloakCluster := getKeycloakClusters()
+	if err := keycloakCluster.ValidateAll(); err != nil {
+		return err
+	}
+
 	if err := cache.Update(nodeID, listener); err != nil {
 		return err
 	}
@@ -78,6 +92,9 @@ func fillCache(cache *xdscache.Cache) error {
 	// 	return err
 	// }
 	if err := cache.Update(nodeID, wsSecriceCluster); err != nil {
+		return err
+	}
+	if err := cache.Update(nodeID, keycloakCluster); err != nil {
 		return err
 	}
 
@@ -92,42 +109,42 @@ func fillCache(cache *xdscache.Cache) error {
 *
 */
 
-func getLocalServiceClusters() *clusterv3.Cluster {
-	return &clusterv3.Cluster{
-		Name:                 local_service,
-		ClusterDiscoveryType: &clusterv3.Cluster_Type{Type: clusterv3.Cluster_STRICT_DNS},
-		LbPolicy:             clusterv3.Cluster_ROUND_ROBIN,
-		LoadAssignment:       getLoadAssignmentForLocalService(),
-	}
-}
+// func getLocalServiceClusters() *clusterv3.Cluster {
+// 	return &clusterv3.Cluster{
+// 		Name:                 local_service,
+// 		ClusterDiscoveryType: &clusterv3.Cluster_Type{Type: clusterv3.Cluster_STRICT_DNS},
+// 		LbPolicy:             clusterv3.Cluster_ROUND_ROBIN,
+// 		LoadAssignment:       getLoadAssignmentForLocalService(),
+// 	}
+// }
 
-func getLoadAssignmentForLocalService() *endpointv3.ClusterLoadAssignment {
-	return &endpointv3.ClusterLoadAssignment{
-		ClusterName: local_service,
-		Endpoints: []*endpointv3.LocalityLbEndpoints{
-			{
-				LbEndpoints: []*endpointv3.LbEndpoint{
-					{
-						HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
-							Endpoint: &endpointv3.Endpoint{
-								Address: &corev3.Address{
-									Address: &corev3.Address_SocketAddress{
-										SocketAddress: &corev3.SocketAddress{
-											Address: local_service,
-											PortSpecifier: &corev3.SocketAddress_PortValue{
-												PortValue: 8081,
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
+// func getLoadAssignmentForLocalService() *endpointv3.ClusterLoadAssignment {
+// 	return &endpointv3.ClusterLoadAssignment{
+// 		ClusterName: local_service,
+// 		Endpoints: []*endpointv3.LocalityLbEndpoints{
+// 			{
+// 				LbEndpoints: []*endpointv3.LbEndpoint{
+// 					{
+// 						HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
+// 							Endpoint: &endpointv3.Endpoint{
+// 								Address: &corev3.Address{
+// 									Address: &corev3.Address_SocketAddress{
+// 										SocketAddress: &corev3.SocketAddress{
+// 											Address: local_service,
+// 											PortSpecifier: &corev3.SocketAddress_PortValue{
+// 												PortValue: 8081,
+// 											},
+// 										},
+// 									},
+// 								},
+// 							},
+// 						},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	}
+// }
 
 func getWSServiceClusters() *clusterv3.Cluster {
 	return &clusterv3.Cluster{
@@ -153,6 +170,43 @@ func getLoadAssignmentForWSService() *endpointv3.ClusterLoadAssignment {
 											Address: "127.0.0.1",
 											PortSpecifier: &corev3.SocketAddress_PortValue{
 												PortValue: 8082,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func getKeycloakClusters() *clusterv3.Cluster {
+	return &clusterv3.Cluster{
+		Name:                 "keycloak",
+		ClusterDiscoveryType: &clusterv3.Cluster_Type{Type: clusterv3.Cluster_STRICT_DNS},
+		LbPolicy:             clusterv3.Cluster_ROUND_ROBIN,
+		LoadAssignment:       getLoadAssignmentForKeycloak(),
+	}
+}
+
+func getLoadAssignmentForKeycloak() *endpointv3.ClusterLoadAssignment {
+	return &endpointv3.ClusterLoadAssignment{
+		ClusterName: local_service,
+		Endpoints: []*endpointv3.LocalityLbEndpoints{
+			{
+				LbEndpoints: []*endpointv3.LbEndpoint{
+					{
+						HostIdentifier: &endpointv3.LbEndpoint_Endpoint{
+							Endpoint: &endpointv3.Endpoint{
+								Address: &corev3.Address{
+									Address: &corev3.Address_SocketAddress{
+										SocketAddress: &corev3.SocketAddress{
+											Address: "127.0.0.1",
+											PortSpecifier: &corev3.SocketAddress_PortValue{
+												PortValue: 8083,
 											},
 										},
 									},
@@ -203,6 +257,9 @@ func getRoutes() []*routev3.Route {
 			ClusterSpecifier: &routev3.RouteAction_Cluster{
 				Cluster: ws_service,
 			},
+			HostRewriteSpecifier: &routev3.RouteAction_HostRewriteLiteral{
+				HostRewriteLiteral: "test-crmvoip.asterisk.local",
+			},
 			UpgradeConfigs: []*routev3.RouteAction_UpgradeConfig{
 				{
 					UpgradeType: "websocket",
@@ -220,16 +277,19 @@ func getRoutes() []*routev3.Route {
 			},
 			Action: action,
 			TypedPerFilterConfig: map[string]*anypb.Any{
-				basicAuthFilterName: getDisableAuthBasic(),
+				basicAuthFilterName: getDisableFilter(),
 			},
 		},
 		{
 			Match: &routev3.RouteMatch{
 				PathSpecifier: &routev3.RouteMatch_Prefix{
-					Prefix: "/ws",
+					Prefix: "/disable-oauth2",
 				},
 			},
-			Action: wsAction,
+			Action: action,
+			TypedPerFilterConfig: map[string]*anypb.Any{
+				oauth2FilterName: getDisableFilter(),
+			},
 		},
 		{
 			Match: &routev3.RouteMatch{
@@ -239,8 +299,27 @@ func getRoutes() []*routev3.Route {
 			},
 			Action: wsAction,
 			TypedPerFilterConfig: map[string]*anypb.Any{
-				basicAuthFilterName: getDisableAuthBasic(),
+				basicAuthFilterName: getDisableFilter(),
 			},
+		},
+		{
+			Match: &routev3.RouteMatch{
+				PathSpecifier: &routev3.RouteMatch_Prefix{
+					Prefix: "/ws-disable-oauth2",
+				},
+			},
+			Action: wsAction,
+			TypedPerFilterConfig: map[string]*anypb.Any{
+				oauth2FilterName: getDisableFilter(),
+			},
+		},
+		{
+			Match: &routev3.RouteMatch{
+				PathSpecifier: &routev3.RouteMatch_Prefix{
+					Prefix: "/ws",
+				},
+			},
+			Action: wsAction,
 		},
 		{
 			Match: &routev3.RouteMatch{
@@ -308,7 +387,8 @@ func getHCM() *hcm.HttpConnectionManager {
 			},
 		},
 		HttpFilters: []*hcm.HttpFilter{
-			getAuthBasic(),
+			// getAuthBasic(),
+			getOAuth2(),
 			getRouteFilter(),
 		},
 	}
@@ -332,10 +412,89 @@ func getAuthBasic() *hcm.HttpFilter {
 	}
 }
 
-func getDisableAuthBasic() *anypb.Any {
-	disableAuth, _ := anypb.New(&routev3.FilterConfig{Disabled: true})
+func getOAuth2() *hcm.HttpFilter {
+	oauht2 := &oath2v3.OAuth2{
+		Config: &oath2v3.OAuth2Config{
+			TokenEndpoint: &corev3.HttpUri{
+				Uri: "http://keycloak:8080/realms/master/protocol/openid-connect/token",
+				HttpUpstreamType: &corev3.HttpUri_Cluster{
+					Cluster: "keycloak",
+				},
+				Timeout: &durationpb.Duration{
+					Seconds: 5,
+				},
+			},
+			AuthorizationEndpoint: "http://localhost:18083/realms/master/protocol/openid-connect/auth",
+			RedirectUri:           "http://%REQ(:authority)%/oauth/callback",
+			RedirectPathMatcher: &matcherv3.PathMatcher{
+				Rule: &matcherv3.PathMatcher_Path{
+					Path: &matcherv3.StringMatcher{
+						MatchPattern: &matcherv3.StringMatcher_Exact{
+							Exact: "/oauth/callback",
+						},
+					},
+				},
+			},
+			SignoutPath: &matcherv3.PathMatcher{
+				Rule: &matcherv3.PathMatcher_Path{
+					Path: &matcherv3.StringMatcher{
+						MatchPattern: &matcherv3.StringMatcher_Exact{
+							Exact: "/oauth/signout",
+						},
+					},
+				},
+			},
+			PassThroughMatcher: []*routev3.HeaderMatcher{
+				{
+					Name: "UPGRADE",
+					HeaderMatchSpecifier: &routev3.HeaderMatcher_StringMatch{
+						StringMatch: &matcherv3.StringMatcher{
+							MatchPattern: &matcherv3.StringMatcher_Exact{
+								Exact: "websocket",
+							},
+						},
+					},
+				},
+			},
+			AuthScopes: []string{"openid", "profile", "email", "offline_access"},
+			Credentials: &oath2v3.OAuth2Credentials{
+				ClientId: "test",
+				TokenSecret: &tlsv3.SdsSecretConfig{
+					Name: "token",
+					SdsConfig: &corev3.ConfigSource{
+						ConfigSourceSpecifier: &corev3.ConfigSource_Path{
+							Path: oauth2TokenPath,
+						},
+					},
+				},
+				TokenFormation: &oath2v3.OAuth2Credentials_HmacSecret{
+					HmacSecret: &tlsv3.SdsSecretConfig{
+						Name: "hmac",
+						SdsConfig: &corev3.ConfigSource{
+							ConfigSourceSpecifier: &corev3.ConfigSource_Path{
+								Path: oauth2HMACnPath,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 
-	return disableAuth
+	oauth2Config, _ := anypb.New(oauht2)
+
+	return &hcm.HttpFilter{
+		Name: oauth2FilterName,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: oauth2Config,
+		},
+	}
+}
+
+func getDisableFilter() *anypb.Any {
+	disable, _ := anypb.New(&routev3.FilterConfig{Disabled: true})
+
+	return disable
 }
 
 func getRouteFilter() *hcm.HttpFilter {
